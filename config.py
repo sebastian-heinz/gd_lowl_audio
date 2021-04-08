716db36d51e24c7e3227946a26721595afe3294e
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2021-04-08 16:16:13 +0800

    windows build

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
+supported_platforms = ["windows", "osx", "linuxbsd", "server"]
+
 def can_build(env, platform):
     return True
 
 
 def configure(env):
+    platform = env["platform"]
+
+    if platform not in supported_platforms:
+        raise RuntimeError("GD_LOWL_AUDIO - This module does not currently support building for this platform")
+
     pass
